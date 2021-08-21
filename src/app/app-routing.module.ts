bf2c0299b058515dff92d4f80ce2ef251441e876
Leandro Carlos Pereira <lcpereira.dev@gmail.com>
2021-08-21 10:07:55 -0300

    feat(initial-structure): add project folder structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: '**',
+    loadChildren: () => import('./pages/not-found/not-found.module').then((m) => m.NotFoundModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
